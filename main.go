406c8fe3973917154ce7a23b72c08b51f8635748
wadhahjemai <Wadhah.Jemai@deutschebahn.com>
2024-03-31 22:48:04 +0200

    refactor: project structure

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/Wa4h1h/troute/pkg/trace"
-)
-
 func main() {
-	ips, err := trace.HostnameToIp("facebook.com", trace.IpV6)
-	if err != nil {
-		panic(err)
-	}
 
-	fmt.Println(ips)
 }
